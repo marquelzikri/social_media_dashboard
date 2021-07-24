f7fa402a201eb0aa0a5e62dbb2e18fe923c30df4
marquelzikri <zmarquel@outlook.com>
2021-07-24 11:08:13 +0700

    Added service reducers to store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,24 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+
 import counterReducer from '../features/counter/counterSlice';
+
+import { albumsApi } from '../services/albums';
+import { postsApi } from '../services/posts';
+import { usersApi } from '../services/users';
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+    [postsApi.reducerPath]: postsApi.reducer,
+    [usersApi.reducerPath]: usersApi.reducer,
   },
+  middleware: (getDefaultMiddleware) => (
+    getDefaultMiddleware()
+      .concat(albumsApi.middleware)
+      .concat(postsApi.middleware)
+      .concat(usersApi.middleware)
+  ),
 });
 
 export type AppDispatch = typeof store.dispatch;
