21cdcd9861673b4373a05b12ce13f25e4a325f54
marquelzikri <zmarquel@outlook.com>
2021-07-24 20:22:14 +0700

    Added dashboard breadcrumbs

--- src/app/containers/Dashboard.tsx
+++ src/app/containers/Dashboard.tsx
@@ -1,14 +1,32 @@
+import { Fragment } from 'react';
+import { useLocation } from 'react-router-dom';
+
 import Header from './Header';
 import Sidebar from './Sidebar';
 
 function Dashboard(props: any) {
+  const location = useLocation();
+
   return (
     <div id="dashboard" className="flex w-screen h-screen p-4 overflow-hidden bg-gray-100 max-w-screen-2xl">
       <Sidebar />
       <section className="w-full h-full px-4">
         <Header />
-        <h1 className="text-2xl font-bold">Users</h1>
-        <main className="h-full px-2 py-6 overflow-auto" style={{height: "calc(100% - 6rem)"}}>
+        <nav className="w-full px-2 my-6 font-sans">
+          <ol className="flex text-2xl font-bold list-reset text-grey-dark">
+            {location.pathname.split("/").map((path, index) => (
+              <Fragment key={index}>
+                {path !== "" ? (
+                  <Fragment>
+                    <li>{path}</li>
+                    <li><span className="mx-2">/</span></li>
+                  </Fragment>
+                ) : null}
+              </Fragment>
+            ))}
+            </ol>
+        </nav>
+        <main className="h-full px-2 py-6 overflow-auto" style={{ height: "calc(100% - 9rem)" }}>
           {props.children}
         </main>
       </section>
