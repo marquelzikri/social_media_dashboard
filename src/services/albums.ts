c24134b545256cfdfe2ed5ae7bac721008c708e1
marquelzikri <zmarquel@outlook.com>
2021-07-24 11:07:33 +0700

    Added albums query service

---
+++ src/services/albums.ts
@@ -0,0 +1,23 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+import { baseUrl } from './utils/url';
+
+import { Album, Photo } from '../models/Album';
+
+export const albumsApi = createApi({
+  reducerPath: 'albums',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getUserAlbums: builder.query<Album[], { userId?: number }>({
+      query: ({ userId }) => `albums?userId=${userId}`,
+    }),
+    getAlbumPhotos: builder.query<Photo[], { albumId?: number }>({
+      query: ({ albumId }) => `albums/${albumId}/photos`,
+    }),
+    getPhotoById: builder.query<Photo, { photoId?: number }>({
+      query: ({ photoId }) => `photos/${photoId}`,
+    }),
+  }),
+});
+
+export const { useGetUserAlbumsQuery } = albumsApi;
