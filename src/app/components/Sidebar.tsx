2b6d8e248c6af01a357025e71b2d129a4dac3f1a
marquelzikri <zmarquel@outlook.com>
2021-07-25 15:37:07 +0700

    Added icon to sidebar items

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,7 +1,11 @@
 import { NavLink } from 'react-router-dom';
 import clsx from 'clsx';
 
+import { ReactElement } from 'react';
+import { BiPhotoAlbum } from 'react-icons/bi';
+import { FiUsers } from 'react-icons/fi';
 import { IoCloseSharp } from 'react-icons/io5';
+import { IoIosPaper } from 'react-icons/io';
 
 import { useAppDispatch, useAppSelector } from '../hooks';
 
@@ -10,6 +14,7 @@ import { selectSidebar, toggleSidebar } from '../../features/sidebar/sidebarSlic
 type Route = {
   label: string;
   path: string;
+  icon: ReactElement;
 };
 
 function Sidebar() {
@@ -21,14 +26,17 @@ function Sidebar() {
     {
       label: 'Users',
       path: '/users',
+      icon: <FiUsers />
     },
     {
       label: 'Posts',
       path: '/posts',
+      icon: <IoIosPaper />
     },
     {
       label: 'Albums',
       path: '/albums',
+      icon: <BiPhotoAlbum />
     },
   ];
 
@@ -72,7 +80,7 @@ function Sidebar() {
 }
 
 function SidebarItem(props: Route & {isSidebarOpen: boolean}) {
-  const { label, path,isSidebarOpen } = props;
+  const { label, path, icon, isSidebarOpen } = props;
 
   return (
     <NavLink
@@ -84,10 +92,7 @@ function SidebarItem(props: Route & {isSidebarOpen: boolean}) {
       activeClassName="text-blue-500 border-r-4 border-blue-500 bg-gradient-to-r from-white to-blue-100 dark:from-gray-700 dark:to-gray-800"
     >
       <span className="text-left">
-        <svg width="20" fill="currentColor" height="20" className="w-5 h-5" viewBox="0 0 1792 1792" xmlns="http://www.w3.org/2000/svg">
-          <path d="M1088 1256v240q0 16-12 28t-28 12h-240q-16 0-28-12t-12-28v-240q0-16 12-28t28-12h240q16 0 28 12t12 28zm316-600q0 54-15.5 101t-35 76.5-55 59.5-57.5 43.5-61 35.5q-41 23-68.5 65t-27.5 67q0 17-12 32.5t-28 15.5h-240q-15 0-25.5-18.5t-10.5-37.5v-45q0-83 65-156.5t143-108.5q59-27 84-56t25-76q0-42-46.5-74t-107.5-32q-65 0-108 29-35 25-107 115-13 16-31 16-12 0-25-8l-164-125q-13-10-15.5-25t5.5-28q160-266 464-266 80 0 161 31t146 83 106 127.5 41 158.5z">
-          </path>
-        </svg>
+        {icon}
       </span>
       <span className="mx-4 text-sm font-normal">
         {label}
